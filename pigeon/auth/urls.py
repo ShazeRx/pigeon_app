bdab5bca9b712bb76301c329543ec9b271123435
ShazeRx <perkowskikrzysio@gmail.com>
2021-05-15 08:47:50 +0200

    Added refresh token endpoint

--- pigeon/auth/urls.py
+++ pigeon/auth/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from pigeon.auth.views import RegisterView, LoginView
+from rest_framework_simplejwt import views as jwt_views
+
 """
 Urls for authentication
 """
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
+    path('refresh/', jwt_views.TokenRefreshView.as_view(), name='toke_refresh')
 ]
